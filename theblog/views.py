3317474aa69374450cd399401a0546a4fc00a004
Ion Leu <leuionofficial@gmail.com>
2023-04-02 19:00:50 +0300

    initial commit

---
+++ theblog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+
+# def home(request):
+# 	context = {}
+# 	return render(request, 'home.html', context)
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+	model = Post
+	template_name = 'article_details.html'
+
+
+class AddPostView(CreateView):
+	model = Post
+	template_name = 'add_post.html'
+	fields = '__all__'
+	# fields = ('title', 'body')
